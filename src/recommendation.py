9237f252cd4c22b2dfe920e4e8ecabd1b542239e
DavynRusello <143073714+DavynRusello@users.noreply.github.com>
2024-08-03 22:23:49 +1000

    Update recommendation.py

--- src/recommendation.py
+++ src/recommendation.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
@@ -33,5 +34,4 @@ def recommend(request: RecommendationRequest):
     return RecommendationResponse(recommended_students=recommended_students)
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
